0225b59fa5227852f766209098efe70cff02e0ed
NicolasLavv <122623397+NicolasLavv@users.noreply.github.com>
2023-02-15 20:06:03 +0300

    Update create_kit_name_kit_test.py

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -1,5 +1,4 @@
-import sender_stand_request # импортируем данные пользователя и токен Authorization + набор с сгенерированным токеном из sender_stand_request (тесты запускаются независимо по отдельности и так)
-
+import sender_stand_request # импортируем данные о заказе и треке
 def test_proverka_code_200():
     right_check= sender_stand_request.response2
     assert right_check.status_code==200
