f9cff037c279cdb35902def2bb93996a5f45ac70
NicolasLavv <122623397+NicolasLavv@users.noreply.github.com>
2023-02-15 20:20:29 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -3,14 +3,14 @@ import requests
 import data
 
 def post_new_order(data): # создаём новый заказ, который содержит токен Authorization
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_ORDER,  # url + api запрос на создание пользоваетеля
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_ORDER,  # url + api запрос на создание заказа
                          json = data.user_order)  # указываем тело
 
 response = post_new_order(data)
 
 print (response.json())
 
-def get_an_order_track(): #функция на получение заказа по треку
+def get_an_order_track(): #функция на получение трека
     return requests.get(configuration.URL_SERVICE + configuration.CREATE_USER_TRACK,
                         params={"t":response.json()['track']})
 response2 = get_an_order_track()
